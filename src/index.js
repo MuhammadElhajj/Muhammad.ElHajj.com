16b6e9771deddcbe47bf3ea98dbc52ac68df1fd3
MuhammadElhajj <3muhammad.el.hajj@gmail.com>
2024-12-24 04:14:01 -0900

    fourth commit

--- src/index.js
+++ src/index.js
@@ -1,10 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-
 import App from './App';
 // import Blog from './Blog/Blog';
-
-import {BrowserRouter, Routes, Route} from 'react-router-dom';
+// import {BrowserRouter, Routes, Route} from 'react-router-dom';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
